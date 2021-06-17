09140183b065c9d0b3f80c42b582b9c327133028
zhum <zhumeng@skyvis.com.cn>
2021-06-17 17:45:44 +0800

    Initial commit

---
+++ src/main/java/cn/com/dmg/rabbitmqlearn/RabbitMqLearnApplication.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.rabbitmqlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/dmg/rabbitmqlearn/RabbitMqLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.rabbitmqlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
